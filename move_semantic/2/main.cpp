8a166be20f0444ca7067e567611c2307071657cb
Ilya Berulava <ilyaberulava@icloud.com>
2024-04-23 14:44:03 +0300

    доделал 2 задачу

--- move_semantic/2/main.cpp
+++ move_semantic/2/main.cpp
@@ -24,29 +24,50 @@ public:
         }
         return *this;
     }
-    big_integer& operator+(big_integer& other){
-
+    
+    // вот тут не понял, нужно ли создавать новое большое число? или можно просто строку вернуть 
+    std::string operator+(big_integer& other){
+        int max_str_size = number.size() > other.number.size() ? number.size(): other.number.size();
+        std::string result(max_str_size, '0');   
+        std::string number1 = std::string(max_str_size - number.length(), '0') + number;
+        std::string number2 = std::string(max_str_size - other.number.length(), '0') + other.number;
+        for (int next = 0, i = max_str_size - 1; i >= 0; i--) {
+            int sum = (number1[i] - '0') + (number2[i] - '0') + next;
+            int mod = sum % 10;
+            int div = sum / 10;
+            result[i] = mod + '0';;
+            next = div;
+        }return result;
     }
 
-    big_integer& operator*(int integer){
-
+    std::string operator*(int integer){
+        std::string result;
+        int carry = 0;
+        for (int i = number.size() - 1; i >= 0; --i) {
+            int digit = number[i] - '0';
+            int product = digit * integer + carry;
+            result = std::to_string(product % 10) + result;
+            carry = product / 10;
+        }
+        while (carry) {
+            result = std::to_string(carry % 10) + result;
+            carry /= 10;
+        }return result;
     }
-
+    
     friend std::ostream& operator<<(std::ostream& os, const big_integer& bi) {
         os << bi.number;
         return os;
     }
-
-
 };
-
 
 int main (){
     std::string a = "11232132131";
     auto number1 = big_integer("114575");
     auto number2 = big_integer("78524");
-    number1 = number2;
-    std::cout << number1;
+    auto result = number1 + number2;
+    std::cout << result<<"\n";
+    std::cout << number2 * 23;
 }
 //Задача 2. Большие числа
 //
