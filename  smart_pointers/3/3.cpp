1a77c5051429cc00adc3fe0e6cd257d1f67eca73
Ilya Berulava <ilyaberulava@icloud.com>
2024-05-02 13:51:02 +0300

    исправил 3 номер умных указателей

---  smart_pointers/3/3.cpp
+++  smart_pointers/3/3.cpp
@@ -1,26 +1,25 @@
 #include <iostream>
-#include <exception>
 
 template<class T>
 class unique_ptr{
 private:
     T* ptr = nullptr;
+    
 public:
     unique_ptr(T* &ptr1){
             ptr = ptr1;
             ptr1 = nullptr;
     };
-    unique_ptr(const unique_ptr<T>& other){
-        throw  std::invalid_argument("Нет конструктора копирования");
-    };
-    unique_ptr<T>& operator=(const unique_ptr<T>& other){
-        throw  std::invalid_argument("Нет оператора присваивания");
-    }
+    unique_ptr(const unique_ptr<T>& other) = delete;
+    unique_ptr<T>& operator=(const unique_ptr<T>& other) = delete;
+    
     T& operator*(){
         return *ptr;
     }
     ~unique_ptr(){
-        delete[] ptr;
+        if (ptr != nullptr) {
+            delete ptr;
+        }
     };
     T* release(){
         auto newPtr = this->ptr;
@@ -32,13 +31,9 @@ public:
 int main(int argc, char *argv[]) {
     int* ptrr = new int(5);
     unique_ptr<int> asd(ptrr);
-//    unique_ptr<int> asd1(ptrr);
-//    asd = asd1;
-//    unique_ptr<int> asd2(asd1);
     std::cout <<*asd << std::endl;
-//    std::cout <<": "<<*ptrr << std::endl;
     int* j = asd.release();
-//    std::cout <<*j << std::endl;
+    delete j;
 
 
     return 0;
